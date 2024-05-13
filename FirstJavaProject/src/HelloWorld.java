3af7230b1f9d6c09c5a194005ca49af2fd8bd5f6
Tareq2926 <tareqahmed2926@gmail.com>
2024-05-14 00:43:27 +0600

    Add files via upload
    My first submission

---
+++ FirstJavaProject/src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    String name;
+    HelloWorld(String nami)
+    {
+        name=nami;
+    }
+    public void DispleyInformation()
+    {
+        System.out.println(name);
+    }
+
+}
---
+++ FirstJavaProject/src/HelloWorldRunner.java
@@ -0,0 +1,7 @@
+public class HelloWorldRunner {
+    public static void main(String[] args) {
+
+        HelloWorld num = new HelloWorld("MK.Tareq");
+        num.DispleyInformation();
+    }
+}
